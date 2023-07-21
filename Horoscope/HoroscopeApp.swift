8796a3d4aca7c27828146d376991daf1034b4040
yuragethigh <yourselfmind@yandex.ru>
2023-07-21 23:17:48 +0300

    Initial Commit

---
+++ Horoscope/HoroscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoroscopeApp.swift
+//  Horoscope
+//
+//  Created by Yuriy on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HoroscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
